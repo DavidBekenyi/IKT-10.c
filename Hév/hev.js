a394afb1415df464e36475aaaf5f5d95292e021d
Bekényi Dávid <BekenyiD@kkszki.local>
2022-10-06 13:14:51 +0200

    done

--- Hév/hev.js
+++ Hév/hev.js
@@ -1,3 +1,16 @@
 function szamol() {
-alert("OK");
+
+var indulo = document.getElementById("select1").value;
+var erkezo = document.getElementById("select2").value;
+
+var eredmeny= indulo-erkezo;
+    console.log(typeof(eredmeny));
+
+    if(erkezo=indulo){
+        alert("buta geci vagy");
+    }
+    
+    else{
+        alert(Math.abs(eredmeny));
+    }
 }

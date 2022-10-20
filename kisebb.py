f743080c7832f1f06591b5ac33358998633d3198
Bekényi Dávid <bekenyid@KKSZKI.LOCAL>
2022-10-20 11:47:43 +0200

    4.f

--- kisebb.py
+++
@@ -1,4 +0,0 @@
-szam1 = int(input("Add meg az elso szamot"))
-szam2 = int(input("Add meg az masodik szamot"))
-szam3 = int(input("Add meg az harmadik szamot"))
-
---
+++ szamolasok.py
@@ -0,0 +1,65 @@
+"""
+list = []
+
+szam1 = int(input("Add meg az elso szamot"))
+szam2 = int(input("Add meg az masodik szamot"))
+szam3 = int(input("Add meg az harmadik szamot"))
+
+list.append(szam1)
+list.append(szam2)
+list.append(szam3)
+
+def kisebb():
+    print(min(list))
+
+kisebb()
+"""
+
+"""
+list = []
+
+szam1 = int(input("Add meg az elso szamot"))
+szam2 = int(input("Add meg az masodik szamot"))
+szam3 = int(input("Add meg az harmadik szamot"))
+
+list.append(szam1)
+list.append(szam2)
+list.append(szam3)
+
+def kisebb():
+    print(max(list))
+
+kisebb()
+"""
+
+"""
+x = int(input("Dolgozat pontszama: "))
+
+if x < 50:
+    print("elegtelen")
+
+elif 50 <= x <60 :
+    print("elegsegges")
+
+elif 60 <= x <70 :
+    print("kozepes")
+
+elif 70 <= x <85 :
+    print("jo")
+
+elif x>85 :
+    print("jeles")
+"""
+
+"""
+szam = int(input("szam most"))
+
+if szam % 3 == 0:
+    print("a szam oszthato 3mal")
+
+elif szam % 5 == 0:
+    print("a szam oszthato 5tel")
+
+else:
+    print("a szam paros vagy 1 et irtal kys")
+"""
